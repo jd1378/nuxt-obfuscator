2b43d502b2eed06f5028d0834018471828fa0a14
Javad Mnjd <javad.mnjd@hotmail.com>
2023-12-08 22:18:17 +0330

    fix: improve class match and replace

--- src/build/vite-plugin.ts
+++ src/build/vite-plugin.ts
@@ -3,8 +3,7 @@ import type {Plugin} from 'vite';
 import type {ModuleOptions} from '../module';
 import {stringContains, stringEndsWith} from '../runtime/postcss/utils';
 
-const classMatcher = /(class.*?(["']))(.*?)(?=\2)/gms;
-const classExpressionsMatcher = /(["'])(.*?)(?=\1)/gm;
+const quotesMatcher = /(["'])(.*?)(?=\1)/gm;
 
 export function obfuscatorVitePlugin(
   options: ModuleOptions,
@@ -18,21 +17,21 @@ export function obfuscatorVitePlugin(
       if (stringEndsWith(id, options.defaultExtensions)) {
         try {
           const transformedCode = code.replace(
-            classMatcher,
-            (m: string, p1: string, _: string, p3: string) => {
-              if (m && p3) {
+            quotesMatcher,
+            (m: string, p1: string, p2: string) => {
+              if (m && p2) {
                 let replacement: string;
-                if (classExpressionsMatcher.test(p3)) {
+                if (quotesMatcher.test(p2)) {
                   replacement =
                     p1 +
-                    p3.replace(
-                      classExpressionsMatcher,
+                    p2.replace(
+                      quotesMatcher,
                       (_, ep1, ep2) =>
                         ep1 +
                         ep2.split(/\s+/).map(getObfuscatedClassName).join(' '),
                     );
                 } else {
-                  replacement = `${p1}${p3
+                  replacement = `${p1}${p2
                     .split(/\s+/)
                     .map(getObfuscatedClassName)
                     .join(' ')}`;
